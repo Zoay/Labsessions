d81a359755f79aff85db254f0652035f633c9f1c
Zoay <jkanmogne@gmail.com>
2014-12-08 01:34:52 +0100

    Creating and adding file index.php

---
+++ sessions/1/src/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Creating a search</title>
+</head>
+<body>
+	<div id="wrapper">
+		<input type="text" name="s" placeholder="Search..." />
+		<input type="submit" value=">>" />
+	</div>
+	<div id="result"></div>
+</body>
+</html>
