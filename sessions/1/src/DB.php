56346aa7f80683e49088338d50b8c75a93964776
Zoay <jkanmogne@gmail.com>
2014-12-08 22:04:28 +0100

    DB.php : file handles connection to the database

---
+++ sessions/1/src/DB.php
@@ -0,0 +1,19 @@
+<?php
+
+$serverName = 'localhost';
+$dbname = 'zoay_labsessions';
+$user = 'root';
+$pwd = 'root';
+
+try
+{
+	$db = new PDO("mysql:host=$serverName;dbname=$dbname", $user, $pwd);
+	// Set the PDO Error Mode to exception
+	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+	echo "Connected to the database!";
+} catch (PDOException $e) {
+	echo $e->getMessage();
+}
+
+?>
